67e71141db0e17e0f94003ce7729486bb22a8a9a
rescuernet <om-s@list.ru>
2021-12-21 11:21:14 +0400

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
               "PORT": 5000,
               "NODE_ENV": "production",
               "CLIENT_URL":'http://new.samaraswimming.ru',
-              'API_URL': 'http://api.samaraswimming.ru',
+              'API_URL': 'http://new.samaraswimming.ru/api',
               'DB_URL': 'mongodb+srv://admin:admin@cluster0.qkb97.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
               'JWT_ACCESS_SECRET': 'access-Guzev-fpco-react+node+mongoDB',
               'JWT_REFRESH_SECRET': 'refresh-Guzev-fpco-react+node+mongoDB',
