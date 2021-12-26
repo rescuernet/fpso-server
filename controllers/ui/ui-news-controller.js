1427009993f1d5fecbc0adb233c1f8fce3c3487d
rescuernet <om-s@list.ru>
2021-12-27 01:12:26 +0400

    1

--- controllers/ui/ui-news-controller.js
+++ controllers/ui/ui-news-controller.js
@@ -8,6 +8,7 @@ class uiNewsController {
         const limit = parseInt(req.query.limit)
         try {
             const newsData = await uiNewsService.getNews(page,limit);
+            console.log('newsData',newsData)
             return res.json(newsData);
         } catch (e) {
             next(e);
